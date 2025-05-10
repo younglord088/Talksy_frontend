097b570e5e04ff62a8da821ab653e7718af50416
Yash Panjwani <yashmpanjwani@gmail.com>
2025-05-10 16:29:44 +0530

    fix: update BASE_URL for production environment in axios configuration

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:5001/api" : "https://talksy-theta.vercel.app/api";
+const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:5001/api" : "https://talksy-backend.vercel.app/api";
 
 export const axiosInstance = axios.create({
   baseURL: BASE_URL,
